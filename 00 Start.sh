13ea6438ec11eee5ae0ad6c65e102e786b16a7fb
k0ttee <k0ttee@yandex.ru>
2021-05-18 21:55:12 +0300

    Create 00 Start.sh

---
+++ 00 Start.sh
@@ -0,0 +1,16 @@
+##########
+# конфиг #
+##########
+
+nano .bashrc
+
+export LANG="ru_RU.UTF-8"
+export EDITOR=nano
+PS1="${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
+alias grep="grep --color=auto"
+alias ls="ls -Fa --color"
+alias ll="ls -l --color"
+alias reboot="systemctl reboot"
+alias diskspace="df -H /"
+alias diskinode="df -i /"
+alias purge="sync & echo 3 > /proc/sys/vm/drop_caches"

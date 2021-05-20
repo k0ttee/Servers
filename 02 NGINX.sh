fdf3a98c63db8bc22a8a236e962d2af16fbc2449
k0ttee <k0ttee@yandex.ru>
2021-05-21 00:45:09 +0300

    Update 02 NGINX.sh

--- 02 NGINX.sh
+++ 02 NGINX.sh
@@ -66,3 +66,51 @@ http{
 
 /etc/nginx/sites-enabled/site.ru
 
+#HTTPS_WWW → HTTPS
+server {
+        listen 443 ssl http2;
+        server_name www.site.ru;
+        access_log off;
+        error_log off;
+        ssl_certificate     /etc/letsencrypt/live/www.site.ru/fullchain.pem;
+        ssl_certificate_key /etc/letsencrypt/live/www.site.ru/privkey.pem;
+        return 301 https://site.ru$request_uri;
+}
+#HTTPS
+server {
+        listen 443 ssl http2;
+        listen [::]:443 ssl http2 ipv6only=on;
+        server_name site.ru;
+        root /var/www/web;
+        index index.php;
+        location / {
+                rewrite ^/(.*)$ /index.php;
+        }
+        location ~* \.(jpg|gif|png|ico|css|js|svg|txt|html|json)$ {
+                try_files $uri =404;
+        }
+        location = /index.php {
+                include snippets/fastcgi-php.conf;
+                fastcgi_split_path_info ^(.+\.php)(/.+)$;
+                fastcgi_pass unix:/var/run/php/php7.3-fpm.sock;
+        }
+        #ssl
+        ssl on;
+        ssl_certificate     /etc/letsencrypt/live/site.ru/fullchain.pem;
+        ssl_certificate_key /etc/letsencrypt/live/site.ru/privkey.pem;
+        #принуждение браузеров заходить по https
+        add_header Strict-Transport-Security "max-age=1; includeSubDomains; preload";
+        #браузерный кэш
+        add_header Cache-Control "must-revalidate";
+        #запрет кросс-ориджин кроме
+        add_header 'Access-Control-Allow-Origin' 'google.com';
+        #таймаут кэширования сертификата (не обязательно настраивать)
+        ssl_session_cache shared:SSLCACHE:64m;
+        ssl_session_timeout 24h;
+        keepalive_timeout 120 120;
+        keepalive_requests 128;
+        #версии протоколов
+        ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+        ssl_ciphers ALL:!aNULL:!ADH:!eNULL:!LOW:!EXP:RC4+RSA:+HIGH:+MEDIUM;
+}
+

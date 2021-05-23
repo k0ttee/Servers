9cec5c49c993c5d47079ae36168f1059e886b04b
k0ttee <k0ttee@yandex.ru>
2021-05-24 00:38:12 +0300

    Create 04  PostgreSQL.sh

---
+++ 04  PostgreSQL.sh
@@ -0,0 +1,11 @@
+#############
+# установка #
+#############
+
+# (libllvm10 libpq5 postgresql postgresql-12 postgresql-client-12 postgresql-client-common postgresql-common sysstat)
+apt install postgresql
+
+# (libc-ares2 libevent-2.1-7 pgbouncer)
+apt install pgbouncer
+
+#мудули для пхп тут ставить, ибо относятся к этой субд

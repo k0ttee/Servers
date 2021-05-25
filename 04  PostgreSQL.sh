c0c7b5882cef1a11aff8507781117a9cb926ea86
k0ttee <k0ttee@yandex.ru>
2021-05-26 01:13:55 +0300

    Update 04  PostgreSQL.sh

--- 04  PostgreSQL.sh
+++ 04  PostgreSQL.sh
@@ -5,8 +5,17 @@
 # (libllvm10 libpq5 postgresql postgresql-12 postgresql-client-12 postgresql-client-common postgresql-common sysstat)
 apt install postgresql
 
-# (libc-ares2 libevent-2.1-7 pgbouncer)
-apt install pgbouncer
-
 # ()
 apt install php8.0-pgsql
+
+
+
+
+
+
+#############
+# PgBouncer #
+#############
+
+# (libc-ares2 libevent-2.1-7 pgbouncer)
+apt install pgbouncer

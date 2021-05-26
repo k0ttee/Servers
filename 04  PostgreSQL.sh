a58b5ded8730a77e437b0627668482a925cb8f8b
k0ttee <k0ttee@yandex.ru>
2021-05-27 00:32:22 +0300

    Update 04  PostgreSQL.sh

--- 04  PostgreSQL.sh
+++ 04  PostgreSQL.sh
@@ -2,6 +2,10 @@
 # установка #
 #############
 
+# echo 'deb http://apt.postgresql.org/pub/repos/apt/ buster-pgdg main' >> /etc/apt/sources.list.d/pgdg.list
+# wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+# apt update
+
 # (libllvm10 libpq5 postgresql postgresql-12 postgresql-client-12 postgresql-client-common postgresql-common sysstat)
 apt install postgresql
 
@@ -16,6 +20,9 @@ apt install php8.0-pgsql
 #############
 # настройка #
 #############
+
+#способ шифрования пароля
+echo 'password_encryption = scram-sha-256' >> /etc/postgresql/12/main/postgresql.conf
 
 
 

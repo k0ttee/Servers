d575d61ff9483d4fba1393655397e1cfcc48d820
k0ttee <k0ttee@yandex.ru>
2021-05-27 00:25:21 +0300

    Update 04  PostgreSQL.sh

--- 04  PostgreSQL.sh
+++ 04  PostgreSQL.sh
@@ -5,8 +5,17 @@
 # (libllvm10 libpq5 postgresql postgresql-12 postgresql-client-12 postgresql-client-common postgresql-common sysstat)
 apt install postgresql
 
-# ()
+# (php8.0-pgsql)
 apt install php8.0-pgsql
+
+
+
+
+
+
+#############
+# настройка #
+#############
 
 
 

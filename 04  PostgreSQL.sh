85c451f37d570b02dc712d91d5ab37e4a05726dc
k0ttee <k0ttee@yandex.ru>
2021-05-27 23:10:05 +0300

    Update 04  PostgreSQL.sh

--- 04  PostgreSQL.sh
+++ 04  PostgreSQL.sh
@@ -78,7 +78,7 @@ service pgbouncer restart
 # сериализация #
 ################
 
-echo 'default_transaction_isolation = \\'serializable\\'' >> /etc/postgresql/12/main/postgresql.conf
+echo "default_transaction_isolation = 'serializable'" >> /etc/postgresql/12/main/postgresql.conf
 
 ########################################################################################## перезапуск постгреса
 
